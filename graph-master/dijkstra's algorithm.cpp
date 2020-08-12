47523e2bd32b7ec63468e9003a46e7059fdc5456
Diwanshu Mittal <2018uch1618@mnit.ac.in>
2020-08-12 14:26:42 +0530

    Update dijkstra's algorithm.cpp

--- graph-master/dijkstra's algorithm.cpp
+++ graph-master/dijkstra's algorithm.cpp
@@ -1,6 +1,3 @@
-https://www.geeksforgeeks.org/dijkstras-algorithm-for-adjacency-list-representation-greedy-algo-8/
-
-
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
@@ -9,13 +6,13 @@ void heapify(vector<ll> &v, vector<ll> &dis, ll size, ll root){
     ll l = 2*root + 1;
     ll r = l+1;
     ll i = root;
-    if(r<size && dis[v[r]-1]<dis[v[i]-1]){
+    if(r<size && dis[v[r]]<dis[v[i]]){
         i=r;
     }
-    if(l<size && dis[v[l]-1]<dis[v[i]-1]){
+    if(l<size && dis[v[l]]<dis[v[i]]){
         i=l;
     }
-    if(dis[v[i]-1]<dis[v[root]-1]){
+    if(dis[v[i]]<dis[v[root]]){
         swap(v[i], v[root]);
         heapify(v, dis, size, i);
     }
@@ -28,40 +25,37 @@ void buildHeap(vector<ll> &v, vector<ll> &dis, ll n){
 }
 
 int main() {
-    
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    ll t;
-    cin>>t;
-    while(t--){
-        ll n;
-        cin>>n;
-        vector<ll> v, dis;
-        v.push_back(1);
-        dis.push_back(0);
-        for(ll i=1;i<n;i++){
-            v.push_back(i);
-            dis.push_back(INT_MAX);
-        }
-        while(v.size()!=0){
-            ll j = v[0];
-            swap(v[0], v[v.size()-1]);
-            v.pop_back();
-            if(j+1<=n && dis[j]>dis[j-1]+1){
-                dis[j]=dis[j-1]+1;
-            }
-            if((3*j)<=n && dis[(3*j)-1]>dis[j-1]+1){
-                dis[(3*j)-1]=dis[j-1]+1;
-            }
-            buildHeap(v, dis, v.size());
-        }
-        // for(auto i:dis){
-        //     cout<<i<<" ";
-        // }
-        // cout<<endl;
-        cout<<dis[n-1]<<"\n";
-    }
-	return 0;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll n,m;
+	cin>>n>>m;
+	vector< pair<ll, ll> > A[n];
+	vector<ll> dis(n, 10e9), v;
+	dis[0]=0;
+	for(ll i=0;i<n;i++){
+		v.push_back(i);
+	}
+	for(ll i=0;i<m;i++){
+		ll x,y,w;
+		cin>>x>>y>>w;
+		A[x-1].push_back({y-1, w});
+	}
+	while(v.size()!=0){
+		ll j = v[0];
+		swap(v[v.size()-1], v[0]);
+		v.pop_back();
+		for(auto i:A[j]){
+			if(dis[i.first]>dis[j]+i.second){
+				dis[i.first]=dis[j]+i.second;
+			}
+		}
+		buildHeap(v, dis, v.size());
+	}
+	for(ll i=1;i<n;i++){
+		cout<<dis[i]<<" ";
+	}
+	cout<<endl;
 }
